171f052dc4447123c558890c13dc275a817dc2d9
Rik van Achterberg <rik@programmable.nl>
2018-10-10 14:05:54 +0200

    Calculate costs

--- db.py
+++ db.py
@@ -7,6 +7,10 @@ import time
 db_file = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                        "energy.db")
 
+GAS_PRICE_M3 = 0.66365
+KWH_PRICE_HIGH_RATE = 0.21758
+KWH_PRICE_LOW_RATE = 0.20539
+
 
 def create_db():
     conn = sqlite3.connect(db_file)
@@ -59,10 +63,30 @@ def get_data_points(interval, points):
 
     ret = {}
 
+    result = None
+    power_cost = 0
+    gas_cost = 0
+    gas_usage = 0
+    power_usage = 0
+
     for p_end, p_start in periods:
         c.execute(query.format(p_start, p_end))
         conn.commit()
+        prev_result = result
         result = c.fetchone()
+
+        if result and prev_result:
+            high_rate_power_usage = (prev_result[1] - result[1])
+            low_rate_power_usage = (prev_result[2] - result[2])
+            power_usage = high_rate_power_usage + low_rate_power_usage
+
+            gas_usage = prev_result[3] - result[3]
+
+            high_rate_power_cost = high_rate_power_usage * KWH_PRICE_HIGH_RATE
+            low_rate_power_cost = low_rate_power_usage * KWH_PRICE_LOW_RATE
+            gas_cost = gas_usage * GAS_PRICE_M3
+
+            power_cost = high_rate_power_cost + low_rate_power_cost
 
         try:
             ret[int(float(result[0]))] = {
@@ -70,6 +94,10 @@ def get_data_points(interval, points):
                 'kwh_low_total': result[2],
                 'gas_m3_total': result[3],
                 'kw_current': result[4],
+                'gas_usage': gas_usage,
+                'gas_cost': gas_cost,
+                'power_usage': power_usage,
+                'power_cost': power_cost,
             }
         except TypeError:
             # timestamp was None
