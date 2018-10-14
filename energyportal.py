06c069fc86476e939a135a6773329db5382e7ebb
Rik van Achterberg <rik@programmable.nl>
2018-10-14 17:24:03 +0200

    Calculate gas usage

--- energyportal.py
+++ energyportal.py
@@ -8,6 +8,22 @@ app = Flask(__name__)
 db.create_db()
 
 
+def calculate_usage(datapoints: dict):
+    result = datapoints.copy()
+    sorted_timestamps = sorted(datapoints)
+    prev_timestamp = sorted_timestamps[0]
+
+    for timestamp in sorted_timestamps:
+        result[timestamp]['gas_used'] = (
+                datapoints[timestamp]['gas_m3_total']
+                - datapoints[prev_timestamp]['gas_m3_total']
+        )
+
+        prev_timestamp = timestamp
+
+    return result
+
+
 @app.route("/")
 def index():
     return render_template('index.html')
@@ -16,7 +32,9 @@ def index():
 @app.route("/graph-data/<interval_secs>/<num_datapoints>", methods=["GET"])
 def data_points(interval_secs, num_datapoints):
     ret = db.get_data_points(int(interval_secs), int(num_datapoints))
-    response = make_response(json.dumps(ret), 200)
+    enriched = calculate_usage(ret)
+
+    response = make_response(json.dumps(enriched), 200)
     response.headers['Access-Control-Allow-Origin'] = '*'
     return response
 
